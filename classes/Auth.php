4d6a7a955609485dce06421c8b15dbec3d3c6984
Christina Stoll <christina.stoll@gmx.at>
2021-09-12 20:33:07 +0100

    Added logout methode to class User

--- classes/Auth.php
+++ classes/Auth.php
@@ -6,7 +6,7 @@
 */
 class Auth {
 
-/**
+    /**
      * Log in using the session
      *
      * @return void
@@ -14,5 +14,30 @@ class Auth {
     public static function login() {
         session_regenerate_id(true);
         $_SESSION['is_logged_in'] = true;
+    }
+
+    /**
+     * Log out using the session
+     *
+     * @return void
+     */
+    public static function logout() {
+        $_SESSION = [];
+
+        if (ini_get('session.use_cookies')) {
+            $params = session_get_cookie_params();
+
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params['path'],
+                $params['domain'],
+                $params['secure'],
+                $params['httponly']
+            );
+        }
+
+        session_destroy();
     }
 }
