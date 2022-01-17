3dd6a991af6b30b79047614694128d4d057d698b
Christina Stoll <christina.stoll@gmx.at>
2022-01-17 21:25:58 +0100

    Removed redundant favicon link from page404.php

--- page404.php
+++ page404.php
@@ -7,7 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="img/cl-fav.png">
-    <link rel="icon" type="image/png" href="img/cl-fav.png">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/reset.css">
     <script src="https://kit.fontawesome.com/5782589434.js" crossorigin="anonymous"></script>
